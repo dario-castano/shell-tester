e91c334006f1cbf704ac014a0dec11ed4f2e29c2
rolandoquiroz <rolandoquiroz@gmail.com>
2019-08-29 19:14:03 +0000

    ADDING case of only ls with spaces

---
+++ tests/06/06-only_bin_ls_spaces.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# test simple command /bin/ls by doing it a lot of times c:
+
+command="	/bin/ls
+					/bin/ls
+			/bin/ls
+							/bin/ls
+				/bin/ls
+						/bin/ls
+/bin/ls
+		/bin/ls"
+tmp_file="testing_tmp_file_$RANDOM"
+
+# create a pseudo random file
+touch $tmp_file
+# run command
+echo "$command" | $HSHELL > $OUTPUTFILE 2> /dev/null &
+
+# wait a little bit
+$SLEEP $SLEEPSECONDS
+
+# check the result
+nmatch=`cat $OUTPUTFILE | grep -c "$tmp_file"`
+# check the results
+if [ $nmatch -eq 8 ]; then
+		print_ok
+		TEST_PASSED=$((TEST_PASSED + 1))
+else
+		print_ko
+		TEST_FAILED=$((TEST_FAILED + 1))
+fi
+
+# clean up
+stop_shell
+rm -f $tmp_file
