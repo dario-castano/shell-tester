65fca0c85563a69c837d92f03fee1addbbc4c412
Daransoto <901@holbertonschool.com>
2019-08-29 01:33:17 +0000

    Created test for and operator

--- tests/10/10-Control_operators_and.bash
+++ tests/10/10-Control_operators_and.bash
@@ -1,39 +1,44 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#
+# Test and operator.
 
-# Checks if your shell handle the ; command separator
 
-tmp_file="testing_custom_$RANDOM"
-tmp_file2="testing_real_$RANDOM"
-
-command="ls -l && ls && wrong && ls -a"
+command="ls -l /bin && WRONG && ls -a /bin"
+tmp_file="checker_tmp_file_$RANDOM"
 
 # clean up
 stop_shell
 rm -f $tmp_file
-rm -f $tmp_file2
 
 # create a pseudo random file
 touch $tmp_file
-touch $tmp_file2
-
-# send commands
-echo "$command" | $HSHELL > $tmp_file 2> /dev/null &
-echo "$command" | $OSHELL > $tmp_file2 2> /dev/null &
-
+# run command
+echo "$command" | $HSHELL > $OUTPUTFILE 2> /dev/null &
+echo "$command" | $OSHELL > $tmp_file 2> /dev/null &
 # wait a little bit
 $SLEEP $SLEEPSECONDS
-
 # check the result
-nmatch=`grep -c "$tmp_file" "$tmp_file2"`
+nmatch=`diff $OUTPUTFILE $tmp_file | wc -l`
 if [ $nmatch -eq 0 ]; then
 	   print_ok
-	   TEST_PASSED=$((TEST_PASSED + 1))
+	   TESTS_PASSED=$((TESTS_PASSED + 1))
 else
 	   print_ko
-	   TEST_FAILED=$((TEST_FAILED + 1))
+	   TESTS_FAILED=$((TESTS_FAILED + 1))
+	   if [[ $SHOWERRORS -eq 1 ]]; then
+	   	echo ""
+	   	echo -e "[\033[31m************************\033[37m]"
+	   	echo ">> Real output << "
+	   	cat -e $OUTPUTFILE
+	   	echo ">>**************<<"
+	   	echo ""
+	   	echo "> Expected output: "
+	   	cat -e $tmp_file
+	   	echo ">>**************<<"
+	    echo -e "[\033[31m************************\033[37m]"
+	   fi
 fi
 
 # clean up
 stop_shell
 rm -f $tmp_file
-rm -f $tmp_file2
